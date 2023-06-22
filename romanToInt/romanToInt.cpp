a4adba75aa51da04459cf6b8a02e3e686b932d77
Bishoy P <bishoypramanik@gmail.com>
2023-06-22 15:09:50 -0400

    init commit

---
+++ romanToInt/romanToInt.cpp
@@ -0,0 +1,60 @@
+#include "romanToInt.h"
+using namespace std;
+
+int romanToInt(string s) {
+    char previous_char = s[0];
+    int total = 0;
+    for(char current_char: s)
+    {
+        if(getValue(current_char) <= getValue(previous_char))
+        {
+            total += getValue(current_char);
+        } else{
+            total -= getValue(previous_char);
+            total += getValue(current_char) - getValue(previous_char);
+        }
+        previous_char = current_char;
+    }
+    
+    return total;
+}
+
+int getValue(char c)
+{
+    switch(c){
+        case 'I':
+            return 1;
+            break;
+        case 'V':
+            return 5;
+            break;
+        case 'X':
+            return 10;
+            break;
+        case 'L':
+            return 50;
+            break;
+        case 'C':
+            return 100;
+            break;
+        case 'D':
+            return 500;
+            break;
+        case 'M':
+            return 1000;
+            break;
+        default:
+            return 0;
+    }
+}
+
+
+int main()
+{
+    string s = "LVIII";
+    int expect = 58;
+
+    romanToInt(s);
+    bool pass = expect== romanToInt(s);
+    return 0;
+}
---
+++ romanToInt/romanToInt.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+int romanToInt(string s);
+int getValue(char c);
