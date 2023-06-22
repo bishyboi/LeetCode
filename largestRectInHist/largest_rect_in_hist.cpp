d4c5c5481221bea64d9aedfa0a236030ca083e37
Bishoy P <bishoypramanik@gmail.com>
2023-06-22 15:18:30 -0400

    modified stuff

--- largestRectInHist/largest_rect_in_hist.cpp
+++ largestRectInHist/largest_rect_in_hist.cpp
@@ -1,8 +1,25 @@
 //** https://leetcode.com/problems/largest-rectangle-in-histogram/
+#include "largest_rect_in_hist.h"
+using namespace std;
 
-class Solution {
-public:
-    int largestRectangleArea(vector<int>& heights) {
-        
-    }
-};
+
+int largestRectangleArea(vector<int>& heights) {
+    stack<int> areas
+    return 0;
+}
+
+int getArea(int a, int b)
+{
+    if(a < b)
+        return a*2;
+    else
+        return b*2;
+}
+
+int main()
+{
+    vector<int> heights;
+
+    largestRectangleArea(heights);
+    return 0;
+}
---
+++ largestRectInHist/largest_rect_in_hist.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <vector>
+#include <stack>
+using namespace std;
+
+int largestRectangleArea(vector<int> &heights);
+int getArea(int a, int b);
